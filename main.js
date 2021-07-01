1804d137df54cfea7995cfc5c86b2f8485b195a1
yuichi oyama <yuichi19991208@gmail.com>
2021-07-02 00:11:28 +0900

    portfolio作成

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // メニューバー
 $('#toggle').click(function() {
-   $(this).toggleClass("active");
-   $('#overlay').toggleClass("open");
-   if ($("html div").not("#overlay").hasClass("blur")){
-      $("html div").not("#overlay").not(".button_container").removeClass("blur")
-   }else{
-      $("html div").not("#overlay").not(".button_container").addClass("blur")
-   }
+  $(this).toggleClass("active");
+  $('#overlay').toggleClass("open");
+  if ($("html div").not("#overlay").hasClass("blur")){
+    $("html div").not("#overlay").not(".button_container").removeClass("blur")
+  }else{
+    $("html div").not("#overlay").not(".button_container").addClass("blur")
+  }
 });
 
 // 特定の位置までスクロール
